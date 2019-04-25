9cdafae8515923d39949afa1ebf1a9ac542bd0e4
Felipe Augusto Maia <felipe.m992@gmail.com>
2019-04-25 04:32:59 -0300

    Update Teste.scala

--- src/main/scala/humble/model/Teste.scala
+++ src/main/scala/humble/model/Teste.scala
@@ -1,8 +1,9 @@
 package humble.model
 
 import java.lang.{ Long => JLong }
+import java.util.{ List => JList, ArrayList => JAList }
 import org.apache.log4j.Logger
-import javax.persistence.{ Column, Entity, Id, GeneratedValue, Table }
+import javax.persistence.{ Column, Entity, Id, GeneratedValue, Table, OneToMany, JoinColumn, FetchType, ManyToOne }
 import humble.framework.{ ActiveRecordModel, ActiveRecordCompanion, ActiveRecordRest, ActiveRecordJob }
 
 @Entity
@@ -16,6 +17,10 @@ class Teste extends ActiveRecordModel {
 
   @Column(name = "DESCRICAO", length = 16384, nullable = false)
   var descricao: String = _
+  
+  @OneToMany(fetch = FetchType.EAGER)
+  @JoinColumn(name = "ID_FILHO_1")
+  var filhos: JList[Filho] = null
 
 }
 
@@ -23,15 +28,37 @@ object Teste extends ActiveRecordCompanion[Teste]
 
 class TesteRest extends ActiveRecordRest[Teste]
 
+/*
 class TesteJob extends ActiveRecordJob {
 
   override def executar = {
-    var t = new Teste
-    t.descricao = java.util.Calendar.getInstance.getTime.toString
-    t.salvar
-    this.logger.info(s"Total de registros: ${Teste.contarTodos}")
+    var f = new Filho
+    f.aaa = java.util.Calendar.getInstance.getTime.toString
+    f.teste = Teste.listarTodos(registros = 10, pagina = 1)(1)
+    f.salvar
   }
 
   override def expressaoCronFrequenciaExecucao: String = "0 0/1 * 1/1 * ? *"
 
 }
+*/
+
+@Entity
+@Table(name = "FILHO", schema = "PUBLIC")
+class Filho extends ActiveRecordModel {
+
+  @Id
+  @GeneratedValue
+  @Column(name = "ID_FILHO_2")
+  var idFilho: JLong = _
+
+  @Column(name = "AAA", length = 16384, nullable = false)
+  var aaa: String = _
+  
+  @ManyToOne(fetch = FetchType.EAGER)
+  var teste: Teste = null
+
+}
+object Filho extends ActiveRecordCompanion[Filho]
+
+class FilhoRest extends ActiveRecordRest[Filho]
